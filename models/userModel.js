7dc315fb3e4fcac391c361762fe18c140d97e3ed
mayssabargaoui1 <mayssabargaoui@esprit.tn>
2022-08-18 00:46:57 +0100

    User Registration

---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+//https://res.cloudinary.com/smartparkhere/image/upload/v1660688257/samples/VIP/actress-avatar-female-girl-user-woman-icon-477680_jdpfdz.png
+
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name!"],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email!"],
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password!"]
+    },
+    role: {
+        type: Number,
+        default: 0 // 0 = user, 1 = admin
+    },
+    avatar: {
+        type: String,
+        default: "https://res.cloudinary.com/smartparkhere/image/upload/v1660688257/samples/VIP/actress-avatar-female-girl-user-woman-icon-477680_jdpfdz.png"
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model("Users", userSchema)
