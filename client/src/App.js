68a60fad4d1a893c9ffe2a96f554559346170bea
mayssabargaoui1 <mayssa.bargaoui@esprit.tn>
2022-08-23 02:23:22 +0100

    Home UI

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,18 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import Header from './components/header/Header'
 import Body from './components/body/Body'
 
+import './App.css';
+
 function App() {
   return (
     <Router>
     <div className="App">
       <Header>
         <Body>
-          
+
         </Body>
       </Header>
-      Hello
+      
     </div> 
     </Router>
   );
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,29 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+
+import ai from '../../assets/ai.png';
+import './header.css';
+
 
 
 function Header() {
 
 
-    
 
-    return (
-        <header>
-            <div className="logo">
-                <h1><Link to="/">DevAT✮Shop</Link></h1>
-            </div>
 
-            <ul style={transForm}>
-                <li><Link to="/"><i className="fas fa-shopping-cart"></i> Cart</Link></li>
-                {
-                    isLogged
-                    ? userLink()
-                    :<li><Link to="/login"><i className="fas fa-user"></i> Sign in</Link></li>
-                }
-                
-            </ul>
-        </header>
-    )
+
+
+
+return  (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">Let&apos;s Make Finding Your Parking Spot Easier</h1>
+        <p>By reserving a park in advance, you can plan your commute knowing you already have a spot in our parking lot.
+        You'll arrive on time, stress-free, and ready to work. </p>
+  
+        <div className="gpt3__header-content__input">
+          <button type="button">Get Started</button>
+        </div>
+      </div>
+  
+      <div className="gpt3__header-image">
+        <img src={ai} />
+      </div>
+    </div>
+  );
 }
-
-export default Header
+  
+  export default Header;
+  
