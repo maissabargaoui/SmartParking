f7b9f8a5f52d7038d660625e9c6d098f6839288a
mayssabargaoui1 <mayssa.bargaoui@esprit.tn>
2022-08-24 04:02:27 +0100

    Mail Activation

--- client/src/components/body/auth/Register.js
+++ client/src/components/body/auth/Register.js
@@ -5,26 +5,25 @@ import {showErrMsg, showSuccessMsg} from '../../utils/notification/Notification'
 import {isEmpty, isEmail, isLength, isMatch} from '../../utils/validation/Validation'
 
 
-
-
-
 const initialState = {
-    name:'',
+    name: '',
     email: '',
     password: '',
     cf_password: '',
     err: '',
     success: ''
-} 
+}
 
 function Register() {
     const [user, setUser] = useState(initialState)
-    const {name ,email, password,cf_password, err, success} = user
-   
+
+    const {name, email, password,cf_password, err, success} = user
+
     const handleChangeInput = e => {
         const {name, value} = e.target
         setUser({...user, [name]:value, err: '', success: ''})
     }
+
 
     const handleSubmit = async e => {
         e.preventDefault()
@@ -52,34 +51,37 @@ function Register() {
         }
     }
 
-    
     return (
         <div className="login_page">
             <h2>Register</h2>
             {err && showErrMsg(err)}
             {success && showSuccessMsg(success)}
+
             <form onSubmit={handleSubmit}>
-            <div>
+                <div>
                     <label htmlFor="name">Name</label>
                     <input type="text" placeholder="Enter your name" id="name"
-                    value={name} name="name" onChange={handleChangeInput}  />
-                </div>
-            <div>
-                    <label htmlFor="email">Email Address</label>
-                    <input type="text" placeholder="Enter email address" id="email"
-                    value={email} name="email" onChange={handleChangeInput}  />
-                </div>
-               <div>
-                    <label htmlFor="password">Confirm Password</label>
-                    <input type="password" placeholder="Confirm password" id="password"
-                    value={password} name="password" onChange={handleChangeInput}/>
+                    value={name} name="name" onChange={handleChangeInput} />
                 </div>
 
                 <div>
-                    <label htmlFor="cf_password">Password</label>
-                    <input type="cf_password" placeholder="Enter password" id="cf_password"
-                    value={cf_password} name="cf_password" onChange={handleChangeInput}/>
+                    <label htmlFor="email">Email Address</label>
+                    <input type="text" placeholder="Enter email address" id="email"
+                    value={email} name="email" onChange={handleChangeInput} />
                 </div>
+
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder="Enter password" id="password"
+                    value={password} name="password" onChange={handleChangeInput} />
+                </div>
+
+                <div>
+                    <label htmlFor="cf_password">Confirm Password</label>
+                    <input type="password" placeholder="Confirm password" id="cf_password"
+                    value={cf_password} name="cf_password" onChange={handleChangeInput} />
+                </div>
+
                 <div className="row">
                     <button type="submit">Register</button>
                 </div>
@@ -87,7 +89,7 @@ function Register() {
 
             <p>Already an account? <Link to="/login">Login</Link></p>
         </div>
-            )
-            }
+    )
+}
 
 export default Register
